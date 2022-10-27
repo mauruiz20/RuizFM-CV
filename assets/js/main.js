f93a7f3fb92b151286ebd1241386c0388eeb946f
Ruiz, Francisco Mauricio <maumarruiz@hotmail.com>
2022-10-27 13:53:08 -0300

    Botones dinamicos

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,48 @@
+const doc = document; // Abreviación de document
+
+doc.addEventListener("DOMContentLoaded", (e) => {
+  // Delegación de eventos
+  scrollTopButton(".scroll-top-btn");
+  printPDFButton(".print-pdf-btn");
+});
+
+/* Scroll Top Button */
+
+function scrollTopButton(btn) {
+  const $scrollBtn = doc.querySelector(btn);
+  window.addEventListener("scroll", (e) => {
+    let scrollTop = window.pageYOffset || doc.documentElement.scrollTop;
+
+    if (scrollTop > 150) {
+      $scrollBtn.classList.remove("hidden");
+    } else {
+      $scrollBtn.classList.add("hidden");
+    }
+  });
+  doc.addEventListener("click", (e) => {
+    if (e.target.matches(btn)) {
+      window.scrollTo({
+        behavior: "smooth",
+        top: 0,
+      });
+    }
+  });
+}
+
 /* Scale CV */
 function scaleCV() {
-  document.body.classList.add("scale-cv");
+  doc.body.classList.add("scale-cv");
 }
 
 function removeScale() {
-  document.body.classList.remove("scale-cv");
+  doc.body.classList.remove("scale-cv");
 }
 
-// Generate PDF
+/* Generate PDF */
 
-let areaCV = document.getElementById("area-cv");
+let areaCV = doc.getElementById("area-cv");
 
-let printPDF = document.getElementById("print-pdf");
-
-let opt = {
+let options = {
   margin: 1,
   filename: "CV - Ruiz, Francisco Mauricio.pdf",
   image: { type: "jpeg", quality: 0.98 },
@@ -21,19 +50,19 @@ let opt = {
   jsPDF: { format: "a4", orientation: "portrait" },
 };
 
-// Function to call areaCV and Html2Pdf options
-function generateResume() {
-  html2pdf(areaCV, opt);
+/* Print PDF Button */
+
+function printPDFButton(btn) {
+  doc.addEventListener("click", (e) => {
+    if (e.target.matches(btn)) {
+      // 1. The class .scale-cv is added to the body, where it reduces the size of the elements
+      scaleCV();
+
+      // 2. The PDF is generated
+      html2pdf(areaCV, options);
+
+      // 3. The .scale-cv class is removed from the body after 5 seconds to return to normal size.
+      setTimeout(removeScale, 5000);
+    }
+  });
 }
-
-// When the button is clicked, it executes the three functions
-printPDF.addEventListener("click", () => {
-  // 1. The class .scale-cv is added to the body, where it reduces the size of the elements
-  scaleCV();
-
-  // 2. The PDF is generated
-  generateResume();
-
-  // 3. The .scale-cv class is removed from the body after 5 seconds to return to normal size.
-  setTimeout(removeScale, 5000);
-});
